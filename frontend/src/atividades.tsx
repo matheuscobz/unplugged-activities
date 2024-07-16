454c6c07076d93bec7343843d38f41542dbef1ff
Matheus Escobozo <40892771+matheuscobz@users.noreply.github.com>
2024-07-16 19:25:24 -0300

    Alterações no componente
    Componente 'atividades' alterado

--- frontend/src/atividades.tsx
+++ frontend/src/atividades.tsx
@@ -2,6 +2,15 @@ import { Header } from "./header";
 
 export function Atividades(){
   return (
-    <Header />
+    <div>
+      <Header />
+      <main className="flex justify-center bg-gray-100 ">
+        <div className="flex flex-col items-center justify-center min-w-[800px] max-w-[900px] h-full m-10 bg-white p-12 shadow-xl rounded">
+          
+        </div>
+      </main>
+    </div>
+
+    
   )
 }
